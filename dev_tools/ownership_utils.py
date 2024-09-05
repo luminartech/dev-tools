6363c8c5434bd8897b20aa35be5f90a87560cff4
Markus Hofbauer <markus.hofbauer@luminartech.com>
2024-09-05 14:40:07 +0200

    Add codeowner tools (#14)
    Co-authored-by: Chris Bachhuber <cbachhuber89@gmail.com>

---
+++ dev_tools/ownership_utils.py
@@ -0,0 +1,99 @@
+# Copyright (c) Luminar Technologies, Inc. All rights reserved.
+# Licensed under the MIT License.
+
+import re
+import subprocess
+import sys
+from pathlib import Path
+from typing import Dict, Generator, Optional, Tuple
+
+
+class OwnerShipEntry:
+    def __init__(self, pattern: str, owners: Tuple[str, ...], line_number: int) -> None:
+        self.pattern: str = pattern
+        self.owners: Tuple[str, ...] = owners
+        self.line_number: int = line_number
+
+
+class GithubOwnerShip:
+    def __init__(self, repo_dir: Path) -> None:
+        self._ownerships = parse_ownership(repo_dir / ".github" / "CODEOWNERS")
+        self._repo_dir = repo_dir
+        self._cached_regex = CachedRegex()
+
+    def is_owned_by(self, file: Path, codeowner: str) -> bool:
+        return codeowner in self.get_owners(file)
+
+    def get_first_owner(self, file: Path) -> Optional[str]:
+        owners = self.get_owners(file)
+        return owners[0] if owners else None
+
+    def get_owners(self, file: Path) -> Tuple[str, ...]:
+        for ownership in self._ownerships:
+            if self.is_file_covered_by_pattern(file.relative_to(self._repo_dir), ownership.pattern):
+                return ownership.owners
+
+        return ()
+
+    def is_file_covered_by_pattern(self, filepath_in_repo: Path, pattern: str) -> bool:
+        """Implements the complete featureset demonstrated at https://docs.github.com/en/repositories/managing-your-
+        repositorys-settings-and-features/customizing-your-repository/about-code-owners#example-of-a-codeowners-file."""
+        filepath_string = str(filepath_in_repo)
+        if "*" in pattern:
+            return self._match_pattern_with_asterisks(filepath_string, filepath_in_repo.name, pattern)
+        if pattern.startswith("/"):
+            path_from_pattern = Path(pattern[1:].rstrip("/"))
+            return path_from_pattern == filepath_in_repo or path_from_pattern in filepath_in_repo.parents
+        return pattern.rstrip("/") in filepath_string
+
+    def _match_pattern_with_asterisks(self, filepath_string: str, filename: str, pattern: str) -> bool:
+        regex_pattern = pattern.replace("*", "(.*)")
+        regex_pattern = regex_pattern[1:] if pattern.startswith("/") else f".*?{regex_pattern}"
+
+        matches = self._cached_regex.match(regex_pattern, filepath_string)
+
+        if pattern.endswith("/*"):
+            return False if matches is None else bool(matches.groups()[-1] == filename)
+
+        return matches is not None
+
+
+class CachedRegex:
+    """A wrapper around re.match to compile and cache regex patterns.
+
+    It has unlimited size.
+    """
+
+    def __init__(self) -> None:
+        self._cache: Dict[str, re.Pattern[str]] = {}
+
+    def match(self, needle: str, haystack: str, flags: int = 0) -> Optional[re.Match]:
+        if needle not in self._cache:
+            self._cache[needle] = re.compile(needle, flags)
+        return self._cache[needle].match(haystack)
+
+
+def parse_ownership(codeowners_file: Path) -> Tuple[OwnerShipEntry, ...]:
+    """Return ownership in reverse order.
+
+    Order is important. Last matching pattern in CODEOWNERS takes the most
+    precedence.
+    """
+    return tuple(reversed(tuple(get_ownership_entries(codeowners_file))))
+
+
+def get_ownership_entries(codeowners_file: Path) -> Generator[OwnerShipEntry, None, None]:
+    with codeowners_file.open() as file:
+        for line_number, line in enumerate(file.readlines(), start=1):
+            current_line = line.strip()
+
+            if current_line.startswith("#"):  # comment
+                continue
+
+            match = re.findall(r"(\S+)", current_line)
+            if match:
+                yield OwnerShipEntry(match[0], tuple(match[1:]), line_number)
+
+
+def check_git(command: str, repo_dir: Path) -> str:
+    return subprocess.check_output(f"git {command}".split(), cwd=repo_dir).decode(sys.stdout.encoding)
---
+++ tests/test_ownership_utils.py
@@ -0,0 +1,171 @@
+# Copyright (c) Luminar Technologies, Inc. All rights reserved.
+# Licensed under the MIT License.
+
+from pathlib import Path
+
+from pyfakefs.fake_filesystem import FakeFilesystem
+
+from dev_tools.ownership_utils import GithubOwnerShip, get_ownership_entries
+
+
+def _create_repo_path_with_codeowners_file(fs: FakeFilesystem, codeowners_content: str = "") -> Path:
+    repo_dir = Path("repo")
+    codeowners = repo_dir / ".github" / "CODEOWNERS"
+    fs.create_file(codeowners, contents=codeowners_content)
+    return repo_dir
+
+
+def testis_file_covered_by_pattern__matches_file(fs: FakeFilesystem) -> None:
+    repo_dir = _create_repo_path_with_codeowners_file(fs)
+    unit = GithubOwnerShip(repo_dir)
+
+    assert unit.is_file_covered_by_pattern(Path("foo") / "bar" / "file.py", "file.py")
+    assert unit.is_file_covered_by_pattern(Path("foo") / "bar" / "file.py", "foo/bar")
+    assert unit.is_file_covered_by_pattern(Path("a") / "b" / "c" / "d" / "e.txt", "b/c/d")
+    assert unit.is_file_covered_by_pattern(Path("b") / "c" / "d" / "e.txt", "b/c/d")
+
+
+def testis_file_covered_by_pattern__does_not_match_file(fs: FakeFilesystem) -> None:
+    repo_dir = _create_repo_path_with_codeowners_file(fs)
+    unit = GithubOwnerShip(repo_dir)
+
+    assert not unit.is_file_covered_by_pattern(Path("foo") / "bar" / "src" / "CMakeLists.txt", "file.py")
+    assert not unit.is_file_covered_by_pattern(Path("foo") / "bar" / "file.py", "/file.py")
+    assert not unit.is_file_covered_by_pattern(Path("b") / "c" / "f" / "unit.cpp", "b/c/d")
+    assert not unit.is_file_covered_by_pattern(Path("a") / "b" / "c" / "d" / "e.txt", "/b/c/d")
+
+
+def testis_file_covered_by_pattern__wildcard_pattern_matches_file(fs: FakeFilesystem) -> None:
+    repo_dir = _create_repo_path_with_codeowners_file(fs)
+    unit = GithubOwnerShip(repo_dir)
+
+    assert unit.is_file_covered_by_pattern(Path("src") / "team_a_setup" / "install.py", "/src/team_a_*")
+    assert unit.is_file_covered_by_pattern(
+        Path("foo") / "some_specific_aaaa_name" / "src" / "config.yml", "foo/some_*_aaaa_*"
+    )
+    assert unit.is_file_covered_by_pattern(
+        Path("foo") / "some_specific_name_with_more" / "CMakeLists.txt", "foo/some_*_name_*"
+    )
+    assert unit.is_file_covered_by_pattern(
+        Path("src") / "foo" / "some_specific_name_with_more" / "CMakeLists.txt", "foo/some_*_name_*"
+    )
+
+
+def testis_file_covered_by_pattern__match_leading_and_trailing_os_separator(fs: FakeFilesystem) -> None:
+    repo_dir = _create_repo_path_with_codeowners_file(fs)
+    unit = GithubOwnerShip(repo_dir)
+
+    assert unit.is_file_covered_by_pattern(Path("src") / "team_a_setup" / "install.py", "/src/team_a_setup/install.py")
+    assert unit.is_file_covered_by_pattern(Path("src") / "team_a_setup" / "install.py", "src/team_a_setup/install.py")
+    assert unit.is_file_covered_by_pattern(Path("src") / "team_a_setup", "/src/team_a_setup/")
+    assert unit.is_file_covered_by_pattern(Path("src") / "team_a_setup", "src/team_a_setup/")
+
+
+def testis_file_covered_by_pattern__wildcard_pattern_does_not_match_file(fs: FakeFilesystem) -> None:
+    repo_dir = _create_repo_path_with_codeowners_file(fs)
+    unit = GithubOwnerShip(repo_dir)
+
+    assert not unit.is_file_covered_by_pattern(Path("src") / "team_b_setup" / "install.py", "/src/team_a_*")
+    assert not unit.is_file_covered_by_pattern(
+        Path("foo") / "some_specific_aaaa_name" / "config.yml", "foo/some_*_bbb_*"
+    )
+    assert not unit.is_file_covered_by_pattern(
+        Path("foo") / "some_specific_name" / "CMakeLists.txt", "foo/some_*_name_*"
+    )
+    assert not unit.is_file_covered_by_pattern(
+        Path("src") / "foo" / "some_aaaaa_name" / "CMakeLists.txt", "foo/some_bbbb_*"
+    )
+
+
+def testis_file_covered_by_pattern__wildcard_for_non_recursive_ownership(fs: FakeFilesystem) -> None:
+    repo_dir = _create_repo_path_with_codeowners_file(fs)
+    unit = GithubOwnerShip(repo_dir)
+
+    assert unit.is_file_covered_by_pattern(Path("src") / "README.md", "src/*")
+    assert unit.is_file_covered_by_pattern(Path("src") / "README.md", "/src/*")
+    assert not unit.is_file_covered_by_pattern(Path("src") / "packages" / "README.md", "/src/*")
+    assert not unit.is_file_covered_by_pattern(Path("src") / "packages" / "README.md", "src/*")
+
+    # Additional asterisk in path
+    assert unit.is_file_covered_by_pattern(Path("src") / "packages" / "CMakeLists.txt", "pa*ges/*")
+    assert not unit.is_file_covered_by_pattern(Path("src") / "packages" / "CMakeLists.txt", "/pa*ges/*")
+    assert unit.is_file_covered_by_pattern(Path("src") / "packages" / "CMakeLists.txt", "/src/pa*ges/*")
+    assert not unit.is_file_covered_by_pattern(
+        Path("src") / "packages" / "package_a" / "CMakeLists.txt", "/src/pa*ges/*"
+    )
+
+
+def testis_file_covered_by_pattern__almighty_wildcard(fs: FakeFilesystem) -> None:
+    repo_dir = _create_repo_path_with_codeowners_file(fs)
+    unit = GithubOwnerShip(repo_dir)
+
+    assert unit.is_file_covered_by_pattern(Path("CONTRIBUTING.md"), "*")
+    assert unit.is_file_covered_by_pattern(Path("src") / "README.md", "*")
+    assert unit.is_file_covered_by_pattern(Path("foo") / "bar" / "file.py", "*")
+
+
+def testis_file_covered_by_pattern__full_path_matches(fs: FakeFilesystem) -> None:
+    repo_dir = _create_repo_path_with_codeowners_file(fs)
+    unit = GithubOwnerShip(repo_dir)
+
+    assert unit.is_file_covered_by_pattern(Path("foo/bar"), "/foo/bar")
+    assert not unit.is_file_covered_by_pattern(Path("foo/other"), "/foo/bar")
+
+
+def test_github_ownership_is_file_owned_by(fs: FakeFilesystem) -> None:
+    repo_dir = _create_repo_path_with_codeowners_file(
+        fs,
+        codeowners_content="""/foo.txt devs
+/docs/ devs management""",
+    )
+    unit = GithubOwnerShip(repo_dir)
+    assert unit.is_owned_by(repo_dir / "foo.txt", "devs")
+    assert not unit.is_owned_by(repo_dir / "foo.txt", "management")
+    assert unit.is_owned_by(repo_dir / "docs", "devs")
+    assert unit.is_owned_by(repo_dir / "docs", "management")
+
+
+def test_github_ownership_get_owners(fs: FakeFilesystem) -> None:
+    repo_dir = _create_repo_path_with_codeowners_file(
+        fs,
+        codeowners_content="""/src/ devs
+/docs/ devs management""",
+    )
+    unit = GithubOwnerShip(repo_dir)
+    assert unit.get_owners(repo_dir / "src") == ("devs",)
+    assert unit.get_owners(repo_dir / "docs") == ("devs", "management")
+    assert unit.get_owners(repo_dir / "scripts") == ()
+
+    assert unit.get_first_owner(repo_dir / "docs") == "devs"
+    assert unit.get_first_owner(repo_dir / "scripts") is None
+
+
+def test_github_ownership_get_owners__second_overwrites_first__should_be_different_owners(
+    fs: FakeFilesystem,
+) -> None:
+    repo_dir = _create_repo_path_with_codeowners_file(
+        fs,
+        codeowners_content="""/foo/bar bar-owner
+/foo/bar/package package-owner""",
+    )
+    unit = GithubOwnerShip(repo_dir)
+    assert unit.get_owners(repo_dir / "foo" / "bar") == ("bar-owner",)
+    assert unit.get_owners(repo_dir / "foo" / "bar" / "package") == ("package-owner",)
+    assert unit.get_owners(repo_dir / "foo" / "bar" / "something_else") == ("bar-owner",)
+
+
+def test_get_ownership_entries_should_be_parsed_correctly(fs: FakeFilesystem) -> None:
+    codeowners = Path("CODEOWNERS")
+    fs.create_file(
+        codeowners,
+        contents="""/src/ devs
+/docs/ devs management
+# comment ignore""",
+    )
+
+    result = list(get_ownership_entries(codeowners))
+
+    expect_entries_found = 2
+    assert len(result) == expect_entries_found
+    assert result[0].owners == ("devs",)
+    assert result[1].owners == ("devs", "management")
