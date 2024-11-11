ab83d6f8c95bb5cfb226301d1e8419f133a51bad
Chris Bayer <christopher.bayer@luminartech.com>
2024-11-11 17:19:53 +0100

    solve toDog false positive

--- tests/test_check_jira_reference_in_todo.py
+++ tests/test_check_jira_reference_in_todo.py
@@ -12,18 +12,9 @@ from dev_tools.check_jira_reference_in_todo import (
     line_has_incorrect_todo,
 )
 
-
 @pytest.mark.parametrize("content", ["TODO(ABC-1234):", "# TODO(ABC-1234):", "TODO(ABC-1234): remove code"])
-def test_line_has_incorrect_todo_for_no_incorrect_todo(content: str) -> None:
+def test_line_has_incorrect_todo_for_correct_todo(content: str) -> None:
     assert not line_has_incorrect_todo(content)
-
-
-@pytest.mark.parametrize(
-    "content", ["todoom", "todo-color", "toDouble()", "setOdometry()", "getOdometry", "to download"]
-)
-def test_line_has_incorrect_todo_for_no_undetected_todo(content: str) -> None:
-    assert not line_has_incorrect_todo(content)
-
 
 @pytest.mark.parametrize(
     "content",
@@ -40,6 +31,12 @@ def test_line_has_incorrect_todo_for_no_undetected_todo(content: str) -> None:
 )
 def test_line_has_incorrect_todo_for_incorrect_todo(content: str) -> None:
     assert line_has_incorrect_todo(content)
+
+@pytest.mark.parametrize(
+    "content", ["todoom", "todo-color", "toDouble()", "setOdometry()", "getOdometry", "to download", "toDog"]
+)
+def test_line_has_incorrect_todo_for_non_jira_todos(content: str) -> None:
+    assert not line_has_incorrect_todo(content)
 
 
 @pytest.mark.parametrize("content", ["TODO(ABC-1234):", "# TODO(ABC-1234):", "TODO(ABC-1234): remove code"])
