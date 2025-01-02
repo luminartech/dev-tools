568e9faf3928a80f219ae0809ef910c3688d86fd
Mateusz Krakowiak <mateusz.krakowiak@luminartech.com>
2025-01-02 15:28:41 +0000

    deny _tests.cpp/cu

--- tests/local/test_check_cpp_tests_pattern.py
+++ tests/local/test_check_cpp_tests_pattern.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 
 from dev_tools.pre_commit_utils import get_hooks_manifest
@@ -13,5 +15,27 @@ def cpp_tests_name_hook() -> dict:
     raise ValueError(msg)
 
 
-def test__hooks_manifest__contains_hook_validating_test_filenames(cpp_tests_name_hook: dict) -> None:
+def test__hook_validating_test_filenames__is_defined_for_files(cpp_tests_name_hook: dict) -> None:
     assert "files" in cpp_tests_name_hook
+
+
+@pytest.mark.parametrize(
+    "filename",
+    [
+        "src/tests/foo_test.cpp",
+        "src/tests/foo_test.cu",
+        "src/tests/pythontest.py",
+        "src/tests/footest.cpp.txt",
+        "src/python_tests/foo.py",
+        "src/python_test/foo.py",
+    ],
+)
+def test__hook_validating_test_filenames__allows_valid_cases(cpp_tests_name_hook: dict, filename: str) -> None:
+    deny_files_pattern: str = cpp_tests_name_hook["files"]
+    assert not re.match(deny_files_pattern, filename)
+
+
+@pytest.mark.parametrize("filename", ["src/tests/footest.cpp", "src/tests/foo_tests.cpp"])
+def test__hook_validating_test_filenames__denies_invalid_cases(cpp_tests_name_hook: dict, filename: str) -> None:
+    deny_files_pattern: str = cpp_tests_name_hook["files"]
+    assert re.match(deny_files_pattern, filename)
