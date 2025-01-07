ab905c484d4823d07ff78ab6f013267c2a2f8c37
Mateusz Krakowiak <122268459+mkrakowiak-luminar@users.noreply.github.com>
2025-01-07 16:37:38 +0100

    Improve cpp unit test naming scheme (#53)
    Added more rules to improve naming consistency. Ensure that all tests
    match `tests/**/*_test.cpp/cu`.
    
    ---------
    
    Co-authored-by: Markus Hofbauer <markus.hofbauer@luminartech.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- README.md
+++ README.md
@@ -73,7 +73,8 @@ Check that all source code files are `snake_case`. We don't want to use `camelCa
 
 ### `check-cpp-and-cu-unit-test-naming-pattern`
 
-Check that all C++ and Cuda unit test files end with `_test.cpp` or `_test.cu`.
+Check that all C++ and Cuda unit test files end with `_test.cpp` or `_test.cu` (no `_tests`)
+and that they're under a `/tests/` folder (not `/test/`).
 
 ### `check-no-dashes`
 
---
+++ tests/local/test_check_cpp_tests_pattern.py
@@ -0,0 +1,44 @@
+import re
+
+import pytest
+
+from dev_tools.pre_commit_utils import get_hooks_manifest
+
+
+@pytest.fixture
+def cpp_tests_name_hook() -> dict:
+    hook_id = "check-cpp-and-cu-unit-test-naming-pattern"
+    for hook in get_hooks_manifest():
+        if hook["id"] == hook_id:
+            return hook
+    msg = f"{hook_id} not found in hooks manifest"
+    raise ValueError(msg)
+
+
+def test__hook_validating_test_filenames__is_defined_for_files(cpp_tests_name_hook: dict) -> None:
+    assert "files" in cpp_tests_name_hook
+
+
+@pytest.mark.parametrize(
+    "filename",
+    [
+        "src/tests/foo_test.cpp",
+        "src/tests/foo_test.cu",
+        "src/tests/local/foo_test.cpp",
+        "src/tests/pythontest.py",
+        "src/tests/footest.cpp.txt",
+        "src/python_tests/foo.py",
+        "src/python_test/foo.py",
+    ],
+)
+def test__hook_validating_test_filenames__allows_valid_cases(cpp_tests_name_hook: dict, filename: str) -> None:
+    deny_files_pattern: str = cpp_tests_name_hook["files"]
+    assert not re.match(deny_files_pattern, filename), "Valid test filename should not match the deny pattern"
+
+
+@pytest.mark.parametrize(
+    "filename", ["src/tests/footest.cpp", "src/tests/foo_tests.cpp", "src/test/foo_test.cpp", "src/foo_test.cu"]
+)
+def test__hook_validating_test_filenames__denies_invalid_cases(cpp_tests_name_hook: dict, filename: str) -> None:
+    deny_files_pattern: str = cpp_tests_name_hook["files"]
+    assert re.match(deny_files_pattern, filename), "Invalid test filename should match the deny pattern"
