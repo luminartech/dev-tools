0af7ff94783c2c91707b790a2897b1b37845c73e
Mateusz Krakowiak <mateusz.krakowiak@luminartech.com>
2025-02-06 15:47:31 +0000

    update the dict with new values

--- dev_tools/sync_vscode_config.py
+++ dev_tools/sync_vscode_config.py
@@ -6,6 +6,7 @@
 from __future__ import annotations
 
 import sys
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
 
@@ -16,18 +17,55 @@ def load_devcontainer_config(devcontainer_json_path: Path) -> Any:  # noqa: ANN4
     return json.loads(devcontainer_json_path.read_text())["customizations"]["vscode"]
 
 
-def update_vscode_config_json(settings_json: Path, settings_to_merge: dict) -> None:
-    settings_json.write_text(json.dumps(settings_to_merge))
+def get_and_set(dict: Any, key: Any, value: Any) -> Any:  # noqa: ANN401
+    old_value = dict.get(key, None)
+    dict[key] = value
+    return old_value
+
+
+@dataclass
+class DictOverwriteRecord:
+    key: str
+    old_value: Any
+    new_value: Any
+
+
+@dataclass
+class ListOverwriteRecord:
+    old_value: Any
+    new_value: Any
+
+
+def update_dict_overwriting_values(dict: Any, new_values_dict: Any) -> list[DictOverwriteRecord]:  # noqa: ANN401
+    overwrite_records = []
+    for key, value in new_values_dict.items():
+        old_value = get_and_set(dict, key, value)
+        if old_value is not None and old_value != value:
+            overwrite_records.append(DictOverwriteRecord(key, old_value, value))
+    return overwrite_records
+
+
+def update_vscode_settings_json(settings_json: Path, settings_dict: dict) -> list[DictOverwriteRecord]:
+    old_settings_dict = json.loads(settings_json.read_text()) if settings_json.is_file() else {}
+    overwrite_records = update_dict_overwriting_values(old_settings_dict, settings_dict)
+    settings_json.write_text(json.dumps(old_settings_dict, indent=2))
+    return overwrite_records
+
+
+def update_vscode_extensions_json(extensions_json: Path, extensions_list: list) -> list[ListOverwriteRecord]:  # noqa: ARG001
+    return []
 
 
 def main() -> int:
+    # Add customizable indentation
+    # print warnings for overwrites
     repo_root = Path.cwd()
     devcontainer_json = repo_root / ".devcontainer" / "devcontainer.json"
     vs_code_folder = repo_root / ".vscode"
 
     devcontainer_config = load_devcontainer_config(devcontainer_json)
-    update_vscode_config_json(vs_code_folder / "settings.json", devcontainer_config["settings"])
-    update_vscode_config_json(vs_code_folder / "extensions.json", devcontainer_config["extensions"])
+    update_vscode_settings_json(vs_code_folder / "settings.json", devcontainer_config["settings"])
+    update_vscode_extensions_json(vs_code_folder / "extensions.json", devcontainer_config["extensions"])
 
     return 0
 
--- tests/test_sync_vscode_config.py
+++ tests/test_sync_vscode_config.py
@@ -1,11 +1,12 @@
 # Copyright (c) Luminar Technologies, Inc. All rights reserved.
 # Licensed under the MIT License.
 
+import re
 from pathlib import Path
 
 import pytest
 
-from dev_tools.sync_vscode_config import load_devcontainer_config
+from dev_tools.sync_vscode_config import DictOverwriteRecord, load_devcontainer_config, update_vscode_settings_json
 
 
 @pytest.fixture
@@ -33,6 +34,10 @@ def simple_devcontainer_json() -> str:
   }
 }
 """
+
+
+def read_text_without_whitespace(file_path: Path) -> str:
+    return re.subn(r"\s", "", file_path.read_text())[0]
 
 
 def test__load_devcontainer_config__loads_extensions(tmp_path: Path, simple_devcontainer_json: str) -> None:
@@ -64,3 +69,35 @@ def test__load_devcontainer_config__loads_json_with_comments(tmp_path: Path) -> 
 """)
     read_config = load_devcontainer_config(devcontainer_json_path)
     assert "extensions" in read_config
+
+
+def test__update_vscode_settings_json__creates_new_settings(tmp_path: Path) -> None:
+    settings_json_path = tmp_path / "settings.json"
+    update_vscode_settings_json(settings_json_path, {"redhat.telemetry.enabled": False})
+    assert settings_json_path.exists()
+    text_without_whitespace = read_text_without_whitespace(settings_json_path)
+    assert text_without_whitespace == '{"redhat.telemetry.enabled":false}'
+
+
+def test__update_vscode_settings_json__updates_existing_settings(tmp_path: Path) -> None:
+    settings_json_path = tmp_path / "settings.json"
+    settings_json_path.write_text('{"redhat.telemetry.enabled":true}')
+
+    overwrite_records = update_vscode_settings_json(settings_json_path, {"redhat.telemetry.enabled": False})
+
+    text_without_whitespace = read_text_without_whitespace(settings_json_path)
+    assert text_without_whitespace == '{"redhat.telemetry.enabled":false}'
+    assert len(overwrite_records) == 1
+    assert overwrite_records[0] == DictOverwriteRecord(key="redhat.telemetry.enabled", old_value=True, new_value=False)
+
+
+def test__update_vscode_settings_json__leaves_custom_settings(tmp_path: Path) -> None:
+    settings_json_path = tmp_path / "settings.json"
+    settings_json_path.write_text('{"another.setting":5}')
+
+    overwrite_records = update_vscode_settings_json(settings_json_path, {"redhat.telemetry.enabled": False})
+
+    text_without_whitespace = read_text_without_whitespace(settings_json_path)
+    assert '"another.setting":5' in text_without_whitespace
+    assert '"redhat.telemetry.enabled":false' in text_without_whitespace
+    assert overwrite_records == []
