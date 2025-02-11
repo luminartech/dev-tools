20966fcbeaa44fadb49fb08582c41571942168c5
Mateusz Krakowiak <mateusz.krakowiak@luminartech.com>
2025-02-11 14:39:50 +0000

    use FakeFilesystem

--- tests/test_sync_vscode_config.py
+++ tests/test_sync_vscode_config.py
@@ -5,6 +5,7 @@ import re
 from pathlib import Path
 
 import pytest
+from pyfakefs.fake_filesystem import FakeFilesystem
 
 from dev_tools.sync_vscode_config import (
     DictOverwriteRecord,
@@ -46,22 +47,22 @@ def read_text_without_whitespace(file_path: Path) -> str:
     return re.subn(r"\s", "", file_path.read_text())[0]
 
 
-def test__load_devcontainer_config__loads_extensions(tmp_path: Path, simple_devcontainer_json: str) -> None:
-    devcontainer_json_path = tmp_path / "devcontainer.json"
+def test__load_devcontainer_config__loads_extensions(fs: FakeFilesystem, simple_devcontainer_json: str) -> None:  # noqa: ARG001
+    devcontainer_json_path = Path("devcontainer.json")
     devcontainer_json_path.write_text(simple_devcontainer_json)
     read_config = load_devcontainer_config(devcontainer_json_path)
     assert "BazelBuild.vscode-bazel" in read_config["extensions"]
 
 
-def test__load_devcontainer_config__loads_settings(tmp_path: Path, simple_devcontainer_json: str) -> None:
-    devcontainer_json_path = tmp_path / "devcontainer.json"
+def test__load_devcontainer_config__loads_settings(fs: FakeFilesystem, simple_devcontainer_json: str) -> None:  # noqa: ARG001
+    devcontainer_json_path = Path("devcontainer.json")
     devcontainer_json_path.write_text(simple_devcontainer_json)
     read_config = load_devcontainer_config(devcontainer_json_path)
     assert ("redhat.telemetry.enabled", False) in read_config["settings"].items()
 
 
-def test__load_devcontainer_config__loads_json_with_comments(tmp_path: Path) -> None:
-    devcontainer_json_path = tmp_path / "devcontainer.json"
+def test__load_devcontainer_config__loads_json_with_comments(fs: FakeFilesystem) -> None:  # noqa: ARG001
+    devcontainer_json_path = Path("devcontainer.json")
     devcontainer_json_path.write_text("""
 {
   // This is a comment
@@ -77,16 +78,16 @@ def test__load_devcontainer_config__loads_json_with_comments(tmp_path: Path) -> 
     assert "extensions" in read_config
 
 
-def test__update_vscode_settings_json__creates_new_settings(tmp_path: Path) -> None:
-    settings_json_path = tmp_path / "settings.json"
+def test__update_vscode_settings_json__creates_new_settings(fs: FakeFilesystem) -> None:  # noqa: ARG001
+    settings_json_path = Path("settings.json")
     update_vscode_settings_json(settings_json_path, {"redhat.telemetry.enabled": False})
     assert settings_json_path.exists()
     text_without_whitespace = read_text_without_whitespace(settings_json_path)
     assert text_without_whitespace == '{"redhat.telemetry.enabled":false}'
 
 
-def test__update_vscode_settings_json__updates_existing_settings(tmp_path: Path) -> None:
-    settings_json_path = tmp_path / "settings.json"
+def test__update_vscode_settings_json__updates_existing_settings(fs: FakeFilesystem) -> None:  # noqa: ARG001
+    settings_json_path = Path("settings.json")
     settings_json_path.write_text('{"redhat.telemetry.enabled":true}')
 
     overwrite_records = update_vscode_settings_json(settings_json_path, {"redhat.telemetry.enabled": False})
@@ -97,8 +98,8 @@ def test__update_vscode_settings_json__updates_existing_settings(tmp_path: Path)
     assert overwrite_records[0] == DictOverwriteRecord(key="redhat.telemetry.enabled", old_value=True, new_value=False)
 
 
-def test__update_vscode_settings_json__leaves_custom_settings(tmp_path: Path) -> None:
-    settings_json_path = tmp_path / "settings.json"
+def test__update_vscode_settings_json__leaves_custom_settings(fs: FakeFilesystem) -> None:  # noqa: ARG001
+    settings_json_path = Path("settings.json")
     settings_json_path.write_text('{"another.setting":5}')
 
     overwrite_records = update_vscode_settings_json(settings_json_path, {"redhat.telemetry.enabled": False})
@@ -109,8 +110,8 @@ def test__update_vscode_settings_json__leaves_custom_settings(tmp_path: Path) ->
     assert overwrite_records == []
 
 
-def test__update_vscode_settings_json__formats_the_json(tmp_path: Path) -> None:
-    settings_json_path = tmp_path / "settings.json"
+def test__update_vscode_settings_json__formats_the_json(fs: FakeFilesystem) -> None:  # noqa: ARG001
+    settings_json_path = Path("settings.json")
 
     settings = {
         "setting1": 1,
@@ -127,24 +128,24 @@ def test__update_vscode_settings_json__formats_the_json(tmp_path: Path) -> None:
     assert setting1_lines != setting2_lines
 
 
-def test__update_vscode_settings_json__adds_newline_at_the_end(tmp_path: Path) -> None:
-    settings_json_path = tmp_path / "settings.json"
+def test__update_vscode_settings_json__adds_newline_at_the_end(fs: FakeFilesystem) -> None:  # noqa: ARG001
+    settings_json_path = Path("settings.json")
     update_vscode_settings_json(settings_json_path, {"setting1": 1})
 
     text = settings_json_path.read_text()
     assert text.endswith("\n")
 
 
-def test__update_vscode_extensions_json__creates_new_recommendations(tmp_path: Path) -> None:
-    extensions_json_path = tmp_path / "extensions.json"
+def test__update_vscode_extensions_json__creates_new_recommendations(fs: FakeFilesystem) -> None:  # noqa: ARG001
+    extensions_json_path = Path("extensions.json")
     update_vscode_extensions_json(extensions_json_path, ["charliermarsh.ruff"])
     assert extensions_json_path.exists()
     text_without_whitespace = read_text_without_whitespace(extensions_json_path)
     assert re.search("recommendations.*charliermarsh.ruff", text_without_whitespace)
 
 
-def test__update_vscode_extensions_json__adds_new_recommendations(tmp_path: Path) -> None:
-    extensions_json_path = tmp_path / "extensions.json"
+def test__update_vscode_extensions_json__adds_new_recommendations(fs: FakeFilesystem) -> None:  # noqa: ARG001
+    extensions_json_path = Path("extensions.json")
     extensions_json_path.write_text('{"recommendations":["charliermarsh.ruff"]}')
 
     update_vscode_extensions_json(extensions_json_path, ["ms-python.python"])
@@ -154,8 +155,8 @@ def test__update_vscode_extensions_json__adds_new_recommendations(tmp_path: Path
     assert re.search("recommendations.*ms-python.python", text_without_whitespace)
 
 
-def test__update_vscode_extensions_json__makes_recommendations_unique(tmp_path: Path) -> None:
-    extensions_json_path = tmp_path / "extensions.json"
+def test__update_vscode_extensions_json__makes_recommendations_unique(fs: FakeFilesystem) -> None:  # noqa: ARG001
+    extensions_json_path = Path("extensions.json")
     extensions_json_path.write_text('{"recommendations":["charliermarsh.ruff"]}')
 
     update_vscode_extensions_json(extensions_json_path, ["charliermarsh.ruff"])
@@ -164,20 +165,26 @@ def test__update_vscode_extensions_json__makes_recommendations_unique(tmp_path: 
     assert len(re.findall("charliermarsh.ruff", text_without_whitespace)) == 1
 
 
-def test__update_vscode_extensions_json__supports_utf8_chars(tmp_path: Path) -> None:
-    extensions_json_path = tmp_path / "extensions.json"
+def test__update_vscode_extensions_json__supports_utf8_chars(fs: FakeFilesystem) -> None:  # noqa: ARG001
+    extensions_json_path = Path("extensions.json")
     update_vscode_extensions_json(extensions_json_path, ["🐍"])
 
     text_without_whitespace = read_text_without_whitespace(extensions_json_path)
     assert "🐍" in text_without_whitespace
 
 
-def test__update_vscode_extensions_json__adds_newline_at_the_end(tmp_path: Path) -> None:
-    extensions_json_path = tmp_path / "extensions.json"
+def test__update_vscode_extensions_json__adds_newline_at_the_end(fs: FakeFilesystem) -> None:  # noqa: ARG001
+    extensions_json_path = Path("extensions.json")
     update_vscode_extensions_json(extensions_json_path, ["ms-python.python"])
 
     text = extensions_json_path.read_text()
     assert text.endswith("\n")
+
+
+def test__update_vscode_extensions_json__creates_vscode_folder(fs: FakeFilesystem) -> None:  # noqa: ARG001
+    extensions_json_path = Path("a_vscode_folder", "extensions.json")
+    update_vscode_extensions_json(extensions_json_path, ["ms-python.python"])
+    assert Path("a_vscode_folder").is_dir()
 
 
 def test__combine_lists_without_duplicates__combines_disjoint_lists() -> None:
